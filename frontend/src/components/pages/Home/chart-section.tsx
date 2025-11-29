f7e2ca8de5b7c62bcae9788ad5575c8c72d2966e
Varinder Pal Singh <Varipaul@gmail.com>
2025-11-29 10:43:23 +0530

    chore: minor improvements

--- frontend/src/components/pages/Home/chart-section.tsx
+++ frontend/src/components/pages/Home/chart-section.tsx
@@ -1,9 +1,11 @@
-import { PieChart } from 'lucide-react';
 import {
+  Cell,
   Pie,
+  PieChart,
   ResponsiveContainer,
   Tooltip,
 } from 'recharts';
+import { useSummary } from '../../../api/summary';
 import Card from '../../ui/card';
 import SixMonthTrendChart from './six-month-trend-chart';
 
@@ -12,12 +14,12 @@ const ChartSection = ({
 }: {
   selectedMonth: string;
 }) => {
-  //   const { data: pieData, isLoading } =
-  //     useSummary(selectedMonth);
+  const { data: pieData, isLoading } =
+    useSummary(selectedMonth);
 
-  //   if (isLoading) {
-  //     return <p className="p-6 text-gray-400">Loading...</p>;
-  //   }
+  if (isLoading) {
+    return <p className="p-6 text-gray-400">Loading...</p>;
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
@@ -25,52 +27,42 @@ const ChartSection = ({
         <h3 className="font-semibold mb-4">
           Category Breakdown (selected month)
         </h3>
-        {/* {!isLoading && !pieData?.byCategory ? (
-          <div className="text-center text-gray-400 py-16">
+        {!isLoading && pieData?.byCategory?.length === 0 ? (
+          <div className="flex justify-center items-center h-[100%] text-gray-400 py-16">
             No data for selected month.
           </div>
-        ) : ( */}
-        <div className="h-64">
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie
-                data={[
-                  { name: 'A1', value: 100 },
-                  { name: 'A2', value: 300 },
-                  { name: 'B1', value: 100 },
-                  { name: 'B2', value: 80 },
-                  { name: 'B3', value: 40 },
-                  { name: 'B4', value: 30 },
-                  { name: 'B5', value: 50 },
-                  { name: 'C1', value: 100 },
-                  { name: 'C2', value: 200 },
-                  { name: 'D1', value: 150 },
-                  { name: 'D2', value: 50 },
-                ]}
-                dataKey="value"
-                nameKey="name"
-                innerRadius={50}
-                outerRadius={90}
-                label
-              />
-
-              {/* {pieData?.byCategory.map(
-                  (category: any) => (
-                    <Cell
-                      key={category.name}
-                      fill={category.color}
-                    />
-                  ),
-                )} */}
-
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-        </div>
-        {/* )} */}
+        ) : (
+          <div className="h-64">
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={pieData?.byCategory}
+                  dataKey="total"
+                  nameKey="name"
+                  innerRadius={50}
+                  outerRadius={90}
+                  label
+                  fill="#82ca9d"
+                  isAnimationActive={true}
+                >
+                  {pieData?.byCategory.map(
+                    (category: any) => (
+                      <Cell
+                        key={category.id}
+                        fill={category.color}
+                      />
+                    ),
+                  )}
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+        )}
       </Card>
-
-      <SixMonthTrendChart />
+      <Card>
+        <SixMonthTrendChart />
+      </Card>
     </div>
   );
 };
