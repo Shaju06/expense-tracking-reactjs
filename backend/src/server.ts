1a75850d68beafeffaf45db619694101a6adc0e4
Varinder Pal Singh <Varipaul@gmail.com>
2025-12-08 13:01:11 +0530

    wip: just debugging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
+console.log('🚀 server.ts loaded');
+console.log('ENV PORT =', process.env.PORT);
 import app from './app';
 
 const PORT = Number(process.env.PORT || 4000);
+console.log(`Starting server on port ${PORT}...`);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on ${PORT}`);
