1c7d5ae755fa52f9d4b4ff5e18483ec7043576da
Varinder Pal Singh <Varipaul@gmail.com>
2025-12-08 11:58:01 +0530

    fix: updated app listen

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { prisma } from './primsa';
 const PORT = Number(process.env.PORT || 4000);
 
 async function main() {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on ${PORT}`);
   });
 }
